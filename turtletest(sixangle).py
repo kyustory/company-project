e77d34579997d3c1727fc272d6368d870db2ce02
kyustory <150775613+kyustory@users.noreply.github.com>
2023-11-14 16:43:41 +0900

    second commit
    두번째 올리는 육각형 그리는 거북이

---
+++ turtletest(sixangle).py
@@ -0,0 +1,17 @@
+import turtle
+
+t=turtle.Pen()
+
+t.shape("turtle")
+
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
